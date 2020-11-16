7b7fbbef414dcf249c45f97ffa3c515e838e69b8
Jaime Valdivia <jaivaldivia@gmail.com>
2020-11-16 19:33:00 +0000

    Calcular impuestos del subtotal con valor fijo

--- kbot.rb
+++ kbot.rb
@@ -2,9 +2,21 @@ class Factura
 	def subtotal(cantidad, precioUnitario)
 		cantidad * precioUnitario
 	end
+
+	def impuestos(subtotal)
+		subtotal * 0.08
+	end
 end
 
 factura = Factura.new
 
-subtotal = factura.subtotal(2,600)
-puts subtotal
+cantidad = 2
+precioUnitario = 600
+
+#Subtotal
+subtotal = factura.subtotal(cantidad, precioUnitario)
+puts "#{cantidad} * $#{precioUnitario} = $#{subtotal}"
+
+#Impuestos
+impuestos = factura.impuestos(subtotal)
+puts "(%8.0) = $#{impuestos}"
